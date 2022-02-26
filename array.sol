c1ee92f026ca4d951681c56256645bc75662ba3b
sam <syed.simanta06@gmail.com>
2022-02-26 13:08:44 +0600

    'pspotFacoryPattern'

--- array.sol
+++ array.sol
@@ -1,4 +1,26 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.7.0;
 
-contract  EnumExample {
+contract  Array {
+
+  uint[] public numbers;
+
+  // add elements
+  constructor () {
+    numbers.push(5);
+    numbers.push(6);
+    numbers.push(9);
+  }
+
+  // update
+  function updateArray(uint _index, uint _updateValue) external {
+    numbers[_index] = _updateValue;
+  }
+
+  // delete an element
+  function deleteArray(uint _index) external {
+    delete numbers[_index];
+  }
+
+
+}
--- map.sol
+++ map.sol
@@ -7,9 +7,12 @@ contract  MappingExample{
     mapping(string => uint) public players;
 
     // map data
-    players['bob'] = 20;
-    players['alice'] = 50;
-    players['moon'] = 10;
+    constructor() {
+        players['bob'] = 20;
+        players['alice'] = 50;
+        players['moon'] = 10;
+        versions['alpha']['raj'] = 100;
+    }
 
     // read 
     function getScore(string memory _player) external view returns(uint) {
@@ -24,5 +27,9 @@ contract  MappingExample{
     // delete
     function deletePlayer(string memory _player) external {
         delete players[_player];
+    }
+
+    function getNestedData(string memory _version, string memory _player) external view returns(uint) {
+        return versions[_version][_player];
     }
 }
